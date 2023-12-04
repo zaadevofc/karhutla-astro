835bf9eea4356a31b6e6b62cea0b571cd31db3f2
zaadevofc <zaadevofc@gmail.com>
2023-12-04 09:05:19 +0700

    astro jozz

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000
